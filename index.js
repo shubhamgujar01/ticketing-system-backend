25e473af464bfd50a7b18043cebce978990d2b86
shubhamgujar01 <shubhamgujar791@gmail.com>
2023-12-27 15:33:59 +0530

    add cors

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const dotenv = require("dotenv");
 
-const ticketRoutes = require('./src/routes/ticketRoutes');
-const authRoutes = require('./src/routes/authRoutes');
-const User = require('./src/models/userModel');
-const AuthService = require('./src/services/authService'); // Import the authService
-
+const ticketRoutes = require("./src/routes/ticketRoutes");
+const authRoutes = require("./src/routes/authRoutes");
+const User = require("./src/models/userModel");
+const AuthService = require("./src/services/authService"); // Import the authService
 
 dotenv.config();
 const app = express();
 
 // MongoDB Connection
-mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DB_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 app.use(express.json());
+app.use(cors());
 
-const mongoURI = process.env.DB_STRING; 
+const mongoURI = process.env.DB_STRING;
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -30,20 +34,26 @@ const authService = new AuthService();
 
 // Passport Configuration
 passport.use(
-  new LocalStrategy({ usernameField: 'username' }, async (username, password, done) => {
-    debugger
-    try {
-      const user = await User.findOne({ username });
-      if (!user) return done(null, false, { message: 'Incorrect username.' });
-      const isValidPassword = await authService.comparePasswords(password, user.password);
-      if (!isValidPassword) return done(null, false, { message: 'Incorrect password.' });
-      return done(null, user);
-    } catch (error) {
-      return done(error);
+  new LocalStrategy(
+    { usernameField: "username" },
+    async (username, password, done) => {
+      debugger;
+      try {
+        const user = await User.findOne({ username });
+        if (!user) return done(null, false, { message: "Incorrect username." });
+        const isValidPassword = await authService.comparePasswords(
+          password,
+          user.password
+        );
+        if (!isValidPassword)
+          return done(null, false, { message: "Incorrect password." });
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
     }
-  })
+  )
 );
-
 
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -68,8 +78,8 @@ passport.use(
 app.use(passport.initialize());
 
 // Routes
-app.use('/api', passport.authenticate('jwt', { session: false }), ticketRoutes);
-app.use('/auth', authRoutes);
+app.use("/api", passport.authenticate("jwt", { session: false }), ticketRoutes);
+app.use("/auth", authRoutes);
 
 // ... (other route configurations)
 
